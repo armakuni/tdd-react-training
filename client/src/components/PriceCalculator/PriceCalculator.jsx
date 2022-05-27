0aa3e458d1bad0bbf08039445da15266b1908a70
Mark Harrington <mark.harrington@armakuni.com>
2022-05-27 12:16:22 +0100

    Add skeleton for PriceCalculator

---
+++ client/src/components/PriceCalculator/PriceCalculator.jsx
@@ -0,0 +1,26 @@
+
+function PriceCalculator(props) {
+    
+    const pizzas = props.pizzas
+    const prices = props.prices
+    const totals = pizzas.map(calculatePizzaCost());
+
+    return <div>
+        Total: £{(totals.reduce((x,y) => x + y, 0)).toFixed(2)}
+    </div>
+
+    function calculatePizzaCost() {
+        return pizza => findPizzaSize(pizza).price 
+        + pizza.toppings.map(findToppingPrice()).map(x => x * findPizzaSize(pizza).toppingPriceMultiplier).reduce((x,y) => x + y, 0);
+
+        function findPizzaSize(pizza) {
+            return prices.sizes.find(x => x.size === pizza.size);
+        }
+
+        function findToppingPrice() {
+            return topping => prices.toppings.find(x => x.topping === topping).price;
+        }
+    }
+}
+
+export default PriceCalculator;
