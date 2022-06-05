6b582aaf1efe0fb9dcf17b38bf03a149b2944520
Tom Oram <tom.oram@armakuni.com>
2022-06-05 17:26:28 +0100

    Remove unnecessary stuff

--- projects/day1-react-tdd-exercise/src/App.test.tsx
+++ projects/day1-react-tdd-exercise/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React TDD Project/i);
   expect(linkElement).toBeInTheDocument();
 });
