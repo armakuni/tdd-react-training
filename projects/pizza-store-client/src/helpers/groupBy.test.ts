5cb9c20d5ec33f8785276cf756ab968c2bddc5e4
Tom Oram <tom.oram@armakuni.com>
2022-06-12 12:25:50 +0100

    Remove unneeded test

--- projects/pizza-store-client/src/helpers/groupBy.test.ts
+++ projects/pizza-store-client/src/helpers/groupBy.test.ts
@@ -1,10 +1,6 @@
 import groupBy from './groupBy';
 
 describe('groupBy()', () => {
-  it('returns an empty object for an empty list', () => {
-    expect(groupBy((_item) => 'nothing', [])).toEqual({});
-  });
-
   it('returns grouped items', () => {
     expect(groupBy((item) => item[0], ['x1', 'y1', 'x2', 'y2'])).toEqual({
       x: ['x1', 'x2'],
