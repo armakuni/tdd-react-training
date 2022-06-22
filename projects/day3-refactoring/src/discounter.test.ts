94c36e537bd00e30910483ef95077968193ad8fe
Tom Oram <tom.oram@armakuni.com>
2022-06-22 13:51:13 +0100

    Add some refactoring test helpers

--- projects/day3-refactoring/src/discounter.test.ts
+++ projects/day3-refactoring/src/discounter.test.ts
@@ -1,6 +1,24 @@
 import calculateDiscount from './discounter';
 
-jest.mock('./database', () => ({ }));
+jest.mock('./database');
+
+// const range = (n: number): number[] => [...Array(n).keys()];
+//
+// const combinations = range(15).flatMap(
+//   (numberOfPremium) => range(15).flatMap(
+//     (numberOfStandard) => range(15).map(
+//       (numberOfBudget) => ({ numberOfPremium, numberOfStandard, numberOfBudget }),
+//     ),
+//   ),
+// );
+//
+// const products: Record<string, Product> = {
+//   premium: { productClass: 'premium', price: 1000 },
+//   standard: { productClass: 'standard', price: 100 },
+//   budget: { productClass: 'budget', price: 10 },
+// };
+//
+// const repeat = <T>(count: number, item: T) => Array(count).fill(item) as T[];
 
 describe('discounter', () => {
   it('should pass', () => {
@@ -9,3 +27,19 @@ describe('discounter', () => {
     calculateDiscount(order, cid);
   });
 });
+
+// describe('helpers', () => {
+//   describe('range', () => {
+//     it('returns a list containing the range', () => {
+//       expect(range(2)).toEqual([0, 1]);
+//       expect(range(5)).toEqual([0, 1, 2, 3, 4]);
+//     });
+//   });
+//
+//   describe('repeat', () => {
+//     it('repeats the item n times', () => {
+//       expect(repeat(2, 'hello')).toEqual(['hello', 'hello']);
+//       expect(repeat(3, 5)).toEqual([5, 5, 5]);
+//     });
+//   });
+// });
