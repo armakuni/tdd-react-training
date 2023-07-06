05ca9e953a787fd663302dd80c93dcff9b9ac78d
Mark Bradley <braddle@gmail.com>
2023-07-06 11:47:39 +0100

    updating old dependecies

--- projects/burrito-store-server/src/index.ts
+++
@@ -1,6 +0,0 @@
-import server from './server';
-
-const port = process.env.SERVER_PORT || 5001;
-
-// eslint-disable-next-line no-console
-server().listen(port, () => console.log(`Running on port ${port}`));
