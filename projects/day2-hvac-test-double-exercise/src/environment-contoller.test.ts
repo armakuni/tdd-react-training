d24b141d98bee5fc6bc79fddf4dca84dc06e452e
Mark Bradley <braddle@gmail.com>
2023-07-06 11:06:01 +0100

    Almost complete solution for HVAC kata

--- projects/day2-hvac-test-double-exercise/src/environment-contoller.test.ts
+++ projects/day2-hvac-test-double-exercise/src/environment-contoller.test.ts
@@ -1,5 +1,178 @@
 import { EnvironmentController, HVAC } from "./hvac";
 
+class fakeHvac implements HVAC {
+
+  temp: number
+
+  public fanState: boolean = false
+
+  public coolerState: boolean = false
+
+  public heaterState: boolean = false
+
+  constructor(temp: number) {
+    this.temp = temp;
+  }
+
+  getTemperature = () => this.temp
+  activateFan = () => this.fanState = true
+  activateCooler = () => this.coolerState = true
+  activateHeater = () => this.heaterState = true
+  deactivateFan = () => this.fanState = false
+  deactivateCooler = () => this.coolerState = false
+  deactivateHeater = () => this.heaterState = false
+  isHeaterOn = () => this.heaterState === true
+  isCoolerOn = () => this.coolerState === true
+}
+
 describe('Environment Control System', () => {
+  it('Given the temperature is 21C then everything should be off on the HVAC', ()=> {
+
+    // given the temperature is 21 c
+    const hvac = new fakeHvac(21)
+    const controller = new EnvironmentController(hvac)
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeFalsy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
+
+  it('Given the temperature is 24C then the fan and cooler should be ON on the HVAC', ()=> {
+
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(24)
+    const controller = new EnvironmentController(hvac)
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeTruthy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeTruthy()
+  })
+
+  it('Given that actual temperature is 18ºC, then heater and fan should be on', ()=> {
+
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(18)
+    const controller = new EnvironmentController(hvac)
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeTruthy()
+    expect(hvac.heaterState).toBeTruthy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
+
+  it('Given the fan and cooler are on and that actual temperature is 22ºC, then everything is off', ()=> {
+
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(22)
+    hvac.coolerState = true
+    hvac.fanState = true
+
+    const controller = new EnvironmentController(hvac)
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeFalsy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
+
+  it('Given the fan and heater are on and that actual temperature is 19ºC, then everything only the fan is on', ()=> {
+
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(19)
+    hvac.heaterState = true
+    hvac.fanState = true
+
+
+    const controller = new EnvironmentController(hvac)
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeTruthy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
+
+  it('Given the fan and heater were turned off 4 ticks ago, then the fan is still on', ()=> {
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(19)
+    hvac.heaterState = true
+    hvac.fanState = true
+
+    const controller = new EnvironmentController(hvac)
+    controller.tick()
+    controller.tick()
+    controller.tick()
+    controller.tick()
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeTruthy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
+
+  it('Given the fan and heater were turned off 5 ticks ago, then everything is off', ()=> {
+
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(19)
+    hvac.heaterState = true
+    hvac.fanState = true
+
+    const controller = new EnvironmentController(hvac)
+
+    controller.tick() // Turn off
+    controller.tick() // 1 minute
+    controller.tick() // 2 minutes
+    controller.tick() // 3 minutes
+    controller.tick() // 4 minutes
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeFalsy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
+
+  it('Given the fan and cooler have been on on and that actual temperature is 23ºC, then only the fan come on', ()=> {
+
+    // given the temperature is 24 c
+    const hvac = new fakeHvac(22)
+    hvac.coolerState = true
+    hvac.fanState = true
+
+    const controller = new EnvironmentController(hvac)
+
+    controller.tick() // Turns off fan and cooler
+
+    hvac.temp = 24
+
+    // When the system cycle occurs
+    controller.tick()
+
+    // then everything should be off
+    expect(hvac.fanState).toBeTruthy()
+    expect(hvac.heaterState).toBeFalsy()
+    expect(hvac.coolerState).toBeFalsy()
+  })
 
 })
--- projects/day2-hvac-test-double-exercise/src/hvac.ts
+++ projects/day2-hvac-test-double-exercise/src/hvac.ts
@@ -1,8 +1,85 @@
 export class EnvironmentController {
 
-  tick() {
+  hvac: HVAC;
+
+  fanOffCountdown: number = 0
+
+  coolerCountdown: number = 0
+
+  constructor(hvac: HVAC) {
+    this.hvac = hvac
   }
 
+  tick() {
+    this.fanOffCountdown--
 
+    if (this.fanOffCountdown == 0) {
+      this.hvac.deactivateFan()
+    }
+
+
+    if (this.isTooHot()) {
+      this.coolRoom();
+    } else if (this.isTooCold()) {
+      this.heatRoom();
+    } else if (this.isOptimum()) {
+      if (this.hvac.isHeaterOn()) {
+        this.hvac.deactivateHeater()
+        // Start count - set to 5
+        this.startFanOffCountdown()
+      } else if (this.hvac.isCoolerOn()) {
+        this.stopCooling()
+      }
+    }
+  }
+
+  private startFanOffCountdown() {
+    this.fanOffCountdown = 5
+  }
+
+  private startCoolerCountdown() {
+    this.coolerCountdown = 3
+  }
+
+  private heatRoom() {
+    this.hvac.activateFan()
+    this.hvac.activateHeater()
+  }
+
+  private coolRoom() {
+    this.hvac.activateFan()
+    if (this.coolerCountdown <= 0) {
+      this.hvac.activateCooler()
+    }
+  }
+
+  private stopCooling() {
+    this.hvac.deactivateCooler()
+    this.startCoolerCountdown()
+    this.hvac.deactivateFan()
+  }
+
+  private isTooCold() {
+    return this.hvac.getTemperature() < 19;
+  }
+
+  private isTooHot() {
+    return this.hvac.getTemperature() > 23;
+  }
+
+  private isOptimum() {
+    return this.hvac.getTemperature() <= 22 || this.hvac.getTemperature() > 19;
+  }
 }
 
+export interface HVAC {
+  getTemperature: () => number
+  activateFan: () => void
+  activateCooler: () => void
+  activateHeater: () => void
+  deactivateFan: () => void
+  deactivateCooler: () => void
+  deactivateHeater: () => void
+  isHeaterOn: () => boolean
+  isCoolerOn: () => boolean
+}
